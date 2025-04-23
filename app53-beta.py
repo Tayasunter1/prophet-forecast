c270b0dd69c19b2fcecd987ec058fae119a523f9
Tayasunter1 <88748789+Tayasunter1@users.noreply.github.com>
2025-04-23 22:41:43 +0530

    Update app53-beta.py

--- app53-beta.py
+++ app53-beta.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from prophet.plot import plot_plotly, plot_components_plotly
+from prophet.plot import plot_components_plotly
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
